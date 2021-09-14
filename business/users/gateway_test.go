28edd2495e13ca9930a10cf0a9d843feac79a713
kevin untrojb <kuntrojb@gmail.com>
2021-09-14 02:20:39 -0300

    users test

--- business/users/gateway_test.go
+++ business/users/gateway_test.go
@@ -1,7 +1,34 @@
 package users
 
-import "testing"
+import (
+	"context"
+	"testing"
 
-func TestGatewayShouldReturnOK(t *testing.T) {
+	"github.com/stretchr/testify/assert"
 
+	"github.com/golang/mock/gomock"
+)
+
+func TestCreateUserShouldReturnOK(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	ctx := context.Background()
+	newID := int64(1)
+	userMock := user{
+		Email:   "asads@gmail.com",
+		Alias:   "robertito",
+		Name:    "roberto",
+		Surname: "asd",
+	}
+	mockDao := NewMockMysqlDao(mockCtrl)
+	mockDao.EXPECT().InsertUser(ctx, userMock).Return(newID, nil)
+
+	gtw := &gateway{mockDao, nil}
+	lastID, err := gtw.Create(ctx, userMock)
+	assert.Nil(t, err)
+	assert.Equal(t, lastID, newID)
+}
+
+func TestGetUserAndWalletShouldReturnOK(t *testing.T) {
 }
--- business/users/handler_test.go
+++ business/users/handler_test.go
@@ -1,1 +1,70 @@
 package users
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHandlerPostOK(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	ctx := context.Background()
+	userID := int64(1)
+	userMock := user{
+		Email:   "asads@gmail.com",
+		Alias:   "robertito",
+		Name:    "roberto",
+		Surname: "asd",
+	}
+	userBytes, err := json.Marshal(userMock)
+	assert.Nil(t, err)
+
+	gtwMock := NewMockGateway(mockCtrl)
+	gtwMock.EXPECT().Create(ctx, userMock).Return(userID, nil)
+	httpUsersHandler := NewHandler(gtwMock)
+	c, _ := gin.CreateTestContext(httptest.NewRecorder())
+
+	c.Request, _ = http.NewRequest(http.MethodPost, "/users", strings.NewReader(string(userBytes)))
+
+	httpUsersHandler.Post(c)
+	assert.Nil(t, err)
+	assert.Equal(t, c.Writer.Status(), http.StatusOK)
+
+}
+
+func TestHandlerGetOK(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	ctx := context.Background()
+	userID := int64(1)
+	userMock := user{
+		ID:      userID,
+		Email:   "asads@gmail.com",
+		Alias:   "robertito",
+		Name:    "roberto",
+		Surname: "asd",
+	}
+
+	gtwMock := NewMockGateway(mockCtrl)
+	gtwMock.EXPECT().Get(ctx, userID).Return(userMock, nil)
+
+	httpUsersHandler := NewHandler(gtwMock)
+	c, _ := gin.CreateTestContext(httptest.NewRecorder())
+	c.Request, _ = http.NewRequest(http.MethodPost, fmt.Sprintf("/users/%d", userID), strings.NewReader(""))
+	c.Params = gin.Params{gin.Param{Key: "user_id", Value: "1"}}
+
+	httpUsersHandler.Get(c)
+	assert.Equal(t, c.Writer.Status(), http.StatusOK)
+
+}
