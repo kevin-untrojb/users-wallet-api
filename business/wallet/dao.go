a74dd2f9324c932968b7f42591bc135cd76ec229
kevin untrojb <kuntrojb@gmail.com>
2021-09-15 14:46:47 -0300

    search query and dao method is finish

--- business/wallet/dao.go
+++ business/wallet/dao.go
@@ -3,6 +3,7 @@ package wallet
 import (
 	"bytes"
 	"context"
+	"database/sql"
 	"fmt"
 
 	"github.com/kevin-untrojb/users-wallet-api/internal/mysql"
@@ -26,73 +27,100 @@ func (d dao) SearchTransactions(ctx context.Context, userID string, params *Sear
 	var response SearchResponse
 	transactionsResults := make([]Transaction, 0)
 
-	query, queryParams, err := CreateSearchQuery(userID, params)
-	if err != nil {
-		// todo log
-		return response, err
-	}
-	ctx, cancel := context.WithTimeout(context.Background(), mysql.MediumTimeout)
-	defer cancel()
-
-	rows, err := d.db.RawQuery(ctx, nil, query, queryParams...)
-	if err != nil {
-		// todo log
-		return response, err
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var rank int
-		currentTransaction := Transaction{}
-		// todo completar
-		err := rows.Scan(&currentTransaction.ID, &currentTransaction.UserID, &currentTransaction.TransactionType)
-
+	err := d.db.WithTransaction(func(trx *sql.Tx) error {
+		searchQuery, searchQueryParams, err := CreateSearchQuery(userID, params, false)
 		if err != nil {
-			// todo compeltar
-			return response, err
+			// todo log
+			return  err
+		}
+		ctx, cancel := context.WithTimeout(ctx, mysql.MediumTimeout)
+		defer cancel()
+
+		rows, err := d.db.RawQuery(ctx, trx, searchQuery, searchQueryParams...)
+		if err != nil {
+			// todo log
+			return  err
+		}
+		defer rows.Close()
+
+		for rows.Next() {
+			currentTransaction := Transaction{}
+			err := rows.Scan(&currentTransaction.ID,
+				&currentTransaction.TransactionType,
+				&currentTransaction.Amount,
+				&currentTransaction.Date,
+				&currentTransaction.CurrencyName)
+
+
+			if err != nil {
+				// todo compeltar
+				return err
+			}
+			transactionsResults = append(transactionsResults,currentTransaction)
+		}
+		response.Results = transactionsResults
+
+		CountQuery, countQueryParams, err := CreateSearchQuery(userID, params, true)
+		if err != nil {
+			// todo log
+			return  err
+		}
+		ctx, cancel = context.WithTimeout(ctx, mysql.MediumTimeout)
+		defer cancel()
+
+		row := d.db.RawQueryRow(ctx, trx, CountQuery, countQueryParams...)
+		err = row.Scan(&response.Paging.Total)
+		if err != nil {
+			return err
 		}
 
-	}
+		return nil
+	})
 
+	return response, err
 }
 
 func (d dao) GetWalletsForUser(ctx context.Context, userID string) {
 	panic("implement me")
 }
 
-func CreateSearchQuery(userID string, params *SearchRequestParams) (string, []interface{}, error) {
+func CreateSearchQuery(userID string, params *SearchRequestParams, isCountQuery bool) (string, []interface{}, error) {
 	var query bytes.Buffer
 	queryParams := make([]interface{}, 0)
 
 	if params == nil {
 		return "", nil, fmt.Errorf("nil params")
 	}
+	if isCountQuery{
+		query.WriteString("SELECT count(*) FROM TRANSACTION t")
+	}else {
+		query.WriteString("SELECT t.ID, t.TRANSACTION_TYPE, t.AMOUNT, t.DATE_CREATED, c.NAME from Transaction")
+	}
 
-	query.WriteString("SELECT q.* FROM (")
-
-	query.WriteString("SELECT m.ID, m.USER_ID m.TRANSACTION_TYPE m.CURRENCY_ID, m.CURRENT_BALANCE, m.DATE_CREATED")
-	query.WriteString(", c.ID AS CURRENCY_ID, c.NAME, c.EXPONENT, pr.")
-	query.WriteString(", DENSE_RANK() OVER (ORDER BY m.DATE_CREATED DESC) rnk")
-
-	query.WriteString(" FROM TRANSACTION AS m INNER JOIN WALLET AS w ON (w.ID = m.WALLET_ID)")
-	query.WriteString(" FROM CURRENCY AS c INNER JOIN WALLET AS w ON (c.ID = m.CURRENCY_ID)")
-
-	query.WriteString(" WHERE m.USER_ID= ? ")
-	queryParams = append(queryParams, userID)
+	query.WriteString("INNER JOIN WALLET w ON t.WALLET_ID = w.ID")
+	query.WriteString("INNER JOIN USER u ON u.ID = w.USER_ID")
+	query.WriteString("INNER JOIN CURRENCY c on c.id = w.CURRENCY_ID")
+	query.WriteString(" WHERE")
 
 	if params.Currency != "" {
-		query.WriteString(" AND c.name=?")
+
+		query.WriteString(" c.NAME=? AND")
 		queryParams = append(queryParams, params.Currency)
 	}
+	query.WriteString(" u.ID =?")
+	queryParams = append(queryParams, userID)
+
 	if params.MovementType != "" {
 		query.WriteString(" AND m.TRANSACTION_TYPE=?")
 		queryParams = append(queryParams, params.MovementType)
 	}
 
-	query.WriteString(" ORDER BY m.DATE_CREATED DESC")
+	if !isCountQuery{
+		query.WriteString(" ORDER BY m.DATE_CREATED DESC")
 
-	query.WriteString(") AS q WHERE q.rnk BETWEEN ? AND ?") // Required for pagination
-	queryParams = append(queryParams, params.Offset+1, params.Offset+params.Limit)
+		query.WriteString(" LIMIT ? OFFSET ?") // Required for pagination
+		queryParams = append(queryParams, params.Offset+1, params.Offset+params.Limit)
+	}
 
 	return query.String(), queryParams, nil
 }
